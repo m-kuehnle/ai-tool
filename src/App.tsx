5edb64696faa15b4c0fa837f168e139f1687c4ec
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-03-30 12:56:58 +0100

    new summarize function

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,41 @@ import "./App.css";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
+
+const SUMARIZE_URL = "http://localhost:5173/summarize";
+
+
 function App() {
   // Zustand zum Speichern des Textes
   const [inputText, setInputText] = useState("");
   const [outputText, setOutputText] = useState("");
-
+  const [summary, setSummary] = useState(""); // TypeScript wird den Typ für summary automatisch ableiten
+  const [isLoading, setIsLoading] = useState(false); 
+  
   // Funktion, die aufgerufen wird, wenn der Button geklickt wird
   const handleClick = () => {
     // Hier wird der im Input-Feld eingegebene Text in den outputText-Zustand gesetzt
     setOutputText(inputText);
+    summarizeText(inputText);
   };
 
   // Funktion zum Aktualisieren des eingegebenen Textzustands
-  const handleInputChange = (e: {
-    target: { value: React.SetStateAction<string> };
-  }) => {
+  const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
     setInputText(e.target.value);
+  };
+
+  const summarizeText = (text: any) => {
+    fetch(SUMARIZE_URL, {
+      method: "POST",
+      body: JSON.stringify({
+        text,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoading(false);
+        setSummary(data.message.content);
+      });
   };
 
   return (
@@ -32,12 +51,14 @@ function App() {
           onChange={handleInputChange}
         />
         <Button onClick={handleClick}>Summarize</Button>
+
       </div>
 
       {outputText && (
         <div className="bg-[#fafafa] text-[#333] p-4 mt-4">
-          <p>{outputText}</p>
+          <p>{summary}</p>
         </div>
+        
       )}
     </>
   );

8c7d6c88cdb9db565c7777ee261153da24056e9b
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-03-30 11:30:46 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  
+
+  return (
+    <>
+      <p>Hallo Welt Hallo Manuel</p>
+      <Button>Click</Button>
+    </>
+  )
+}
+
+export default App

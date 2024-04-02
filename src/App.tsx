17d35204524da9020e84507677fa34128c86dcb1
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-04-02 16:46:25 +0200

    css animation removed

--- src/App.tsx
+++ src/App.tsx
@@ -162,21 +162,6 @@ function App() {
           }}
         />
       </div>
-      {/* Grafische Hintergrundanimation  3 */}
-      <div
-        className="absolute bottom-0 right-0 z-0 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        style={{ overflow: "visible" }}
-      >
-        <div
-          className="relative w-[50rem] h-[50rem] rotate-[30deg] bg-gradient-to-tr from-indigo-600 to-indigo-700 opacity-30"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-            left: "-140%", // Positionierung am linken Rand
-            bottom: "-30%", // Positionierung am unteren Rand
-          }}
-        />
-      </div>
     </>
   );
 }

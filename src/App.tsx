d0387cfbf7e13bc57275174b4404be382dd91292
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-04-02 16:49:43 +0200

    new CSSstyle

--- src/App.tsx
+++ src/App.tsx
@@ -158,7 +158,7 @@ function App() {
             clipPath:
               "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             right: "20%", // Positionierung am rechten Rand
-            bottom: "-30%", // Positionierung am unteren Rand
+            bottom: "-60%", // Positionierung am unteren Rand
           }}
         />
       </div>

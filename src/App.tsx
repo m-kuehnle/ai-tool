bed9c30f692ce83dd226844b8271ad1e818a2639
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-04-05 19:39:14 +0200

    new DarkMode and changed data name of the api function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+//IMPORTS
 import React, { useState } from "react";
 import "./App.css";
 import { Textarea } from "@/components/ui/textarea";
@@ -13,8 +14,10 @@ import { ThemeProvider } from "next-themes";
 import { ModeToggle } from "./components/ui/ModeToggle";
 
 
-
+//API KEY
 const { VITE_OCTOAI_TOKEN } = import.meta.env;
+
+//FUNKTIONEN
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -53,7 +56,6 @@ function App() {
 
   return (
     <>
-      {/* ThemeProvider hinzufügen */}
       <ThemeProvider attribute="class">
         <div className="flex justify-between items-center my-10 mx-4 z-10 relative">
           <h1 className="text-2xl font-semibold text-gray-900 tracking-wide">
@@ -62,7 +64,6 @@ function App() {
           <div className="flex items-center justify-center">
             <div className="flex items-center">
               <img src={icon} alt="icon" className="w-12 h-12 mr-2" />
-              {/* ModeToggle hinzufügen */}
               <ModeToggle />
             </div>
           </div>
@@ -96,6 +97,9 @@ function App() {
                   Summarize Text
                 </Button>
 
+
+
+
                 {showAlert && (
                   <Alert variant="destructive">
                     <AlertCircle className="h-4 w-4" />
@@ -113,6 +117,8 @@ function App() {
                 </div>
               )}
 
+
+
               {outputText && (
                 <div className="bg-gray-100 rounded-md p-4 mx-4 mb-10 z-10 relative">
                   <h2 className="text-lg font-bold mb-2 text-gray-600 ">
@@ -124,6 +130,8 @@ function App() {
                 </div>
               )}
             </TabsContent>
+
+
 
             <TabsContent
               className="text-lg font-bold mb-2 text-indigo-600 relative"
@@ -148,6 +156,8 @@ function App() {
             </TabsContent>
           </Tabs>
         </div>
+
+        
         <div className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" style={{ pointerEvents: "none" }}>
           <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style={{ clipPath: "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" }} />
         </div>

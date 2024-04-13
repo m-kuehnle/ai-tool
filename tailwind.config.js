680d2987c26bb56d005b5b960ec64fd4581635f3
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-04-13 19:39:58 +0200

    issue fix with tailwind data

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,6 @@ module.exports = {
       },
     },
     extend: {
-
-
-
-      
-
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -31,18 +26,18 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        meteor: {
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "meteor": {
           "0%": { transform: "rotate(215deg) translateX(0)", opacity: "1" },
           "70%": { opacity: "1" },
           "100%": {
             transform: "rotate(215deg) translateX(-500px)",
             opacity: "0",
           }
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-        "meteor-effect": "meteor 5s linear infinite",
+        }
       },
       colors: {
         border: "hsl(var(--border))",
@@ -84,22 +79,7 @@ module.exports = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-  }
-}
+};

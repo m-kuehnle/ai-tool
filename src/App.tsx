8d5e91bccd86d8a4431bf870b5fa0df569bd00ed
Manuel Kühnle <ge36sej@mytum.de>
2024-04-20 14:18:23 +0200

    add text input cards

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,15 @@ import PdfInput from "./sections/pdfInput";
 import ImageInput from "./sections/imageInput";
 import Background from "./sections/background";
 import FAQ from "./sections/FAQ";
-import BentoGridDemo from "./sections/examples";
 
 function App() {
   return (
     <>
       <ThemeProvider attribute="class">
+        {/* Header */}
         <Header />
 
+        {/* Tabs */}
         <div className="m-4">
           <Tabs defaultValue="text-input">
             <TabsList>
@@ -39,16 +40,6 @@ function App() {
           </Tabs>
         </div>
 
-        <div className="mt-[100px]">
-          <h3 className="font-bold text-3xl text-indigo-600 ml-[150px] underline sm:mt-[-60px]">Examples</h3>
-
-          <div className="ml-[px] sm:ml-[-100px]">
-            <BentoGridDemo />
-          </div>
-          
-        </div>
-
-
         {/* FAQ Section */}
         <div className="sm:m-40 m-8">
           <h2 className="mb-4 font-bold text-indigo-600 text-3xl mt-[100px] sm:mt-[150px] underline">
@@ -56,8 +47,6 @@ function App() {
           </h2>
           <FAQ />
         </div>
-
-
 
         <Background />
       </ThemeProvider>
---
+++ src/utils/constants.tsx
@@ -0,0 +1,53 @@
+import Mars from "../assets/mars.jpg";
+import Ronaldo from "../assets/ronaldo.jpg";
+import Hund from "../assets/hund.jpg";
+
+interface Item {
+  title: string;
+  description: string;
+  header: JSX.Element;
+  example_text: string;
+}
+
+export const items: Item[] = [
+  {
+    title: "A text about Mars",
+    description: "Click on this card to summarize the text about Mars.",
+    header: (
+      <img
+        src={Mars}
+        alt="Mars"
+        className="w-full h-32 object-cover rounded-xl"
+      />
+    ),
+    example_text:
+      "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury. In English, Mars carries the name of the Roman god of war and is often referred to as the 'Red Planet'.",
+  },
+  {
+    title: "A text about a famous footballer",
+    description:
+      "Click on this card to summarize the text about the footballer.",
+    header: (
+      <img
+        src={Ronaldo}
+        alt="Ronaldo"
+        className="w-full h-32 object-cover rounded-xl"
+      />
+    ),
+    example_text:
+      "Cristiano Ronaldo, often hailed as one of the greatest footballers of all time, has left an indelible mark on the world of soccer. Born on February 5, 1985, in Madeira, Portugal, Ronaldo's journey from humble beginnings to global superstar is a testament to his unparalleled talent, dedication, and relentless work ethic.His illustrious career has seen him excel at some of the biggest clubs in Europe, including Sporting Lisbon, Manchester United, Real Madrid, and Juventus. With his remarkable speed, extraordinary dribbling skills, and lethal finishing ability, Ronaldo has shattered numerous records and won countless individual awards, including five Ballon d'Or titles. Beyond his on-field achievements, Ronaldo's impact transcends football. He is a global icon, admired for his philanthropy, charisma, and leadership both on and off the pitch. Ronaldo's immense popularity extends far beyond the realm of sports, with millions of followers on social media and lucrative endorsement deals with leading brands.Despite facing challenges and criticism throughout his career, Ronaldo's unwavering determination and passion for the game have propelled him to unprecedented heights. Whether he's representing his country on the international stage or dazzling fans with his performances for his club, Ronaldo's legacy as one of football's true legends is firmly cemented in history.",
+  },
+  {
+    title: "A text about a dog",
+    description: "Click on this card to summarize the text about the dog.",
+    header: (
+      <img
+        src={Hund}
+        alt="Hund"
+        className="w-full h-32 object-cover rounded-xl"
+      />
+    ),
+    example_text:
+      "Dogs are domesticated mammals belonging to the family Canidae. They have long been close companions of humans and have adapted to various habitats and tasks throughout evolution. Due to their versatility, dogs are utilized worldwide in various roles, including as pets, working animals, guard dogs, therapy dogs, and hunting dogs.Dogs exhibit a wide range of sizes, colors, shapes, and temperaments, which have arisen through centuries of breeding and selection. Their anatomy includes features such as a strong jaw, pronounced sensory organs (such as sense of smell and hearing), and an adaptable spine that allows them to move easily and fulfill various tasks.Communication among dogs primarily occurs through body language, vocalizations, and scent marking. They possess a complex social structure and can interact with both each other and humans. Proper training and socialization are crucial for a dog's behavior and development.Dogs require a balanced diet, regular exercise, medical care, and appropriate grooming to stay healthy and happy. The responsibility that comes with owning a dog should be carefully considered and respected to ensure their needs are met and to foster a positive relationship between human and animal.",
+  },
+];

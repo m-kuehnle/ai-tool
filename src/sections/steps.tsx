d551e74a4c367f3fd363c0956e95bf039813c05d
Leo17220310 <125191929+Leo17220310@users.noreply.github.com>
2024-05-01 14:53:24 +0200

    smooth scrolling to the summarizer

--- src/sections/steps.tsx
+++ src/sections/steps.tsx
@@ -2,6 +2,15 @@ import project from "@/assets/project.svg";
 import { Button } from "@/components/ui/button";
 
 export function Steps() {
+
+  const scrollToSummarizer = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    const summarizerElement = document.getElementById("summarizer");
+    if (summarizerElement) {
+      summarizerElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div>
       <div className="max-w-5xl px-4 xl:px-0 py-10 lg:pt-20 lg:pb-20 mx-auto">
@@ -104,9 +113,9 @@ export function Steps() {
                 </p>
               </div>
             </div>
-            <a href="#summarizer">
-              <Button variant="outline">Use it now</Button>
-            </a>
+            <a href="#" onClick={scrollToSummarizer}>
+          <Button variant="outline">Use it now</Button>
+        </a>
           </div>
         </div>
       </div>
